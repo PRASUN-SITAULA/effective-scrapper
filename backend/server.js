37a966cc1e61257a59e26d485107cc018907e6f4
Prasun Sitaula <psitaula209@gmail.com>
2024-12-11 07:27:01 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@ dotenv.config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log("welcome to the server.Running on port " + PORT);
 });
